f51e206d3a7d6222cf6346b98fd9a79089ae710e
Simon Lindsay <simon.lindsay@adelaide.edu.au>
2017-11-16 21:45:29 +1030

    Get the namespace from the git remote url

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-NAMESPACE="uofa"
+NAMESPACE=$(git remote get-url origin | awk -F'[/:]' '{ print $2 }')
 DIR=$(basename $(pwd))
 BRANCH=$(basename $(git symbolic-ref HEAD 2>/dev/null))
 TAG="${NAMESPACE}/${DIR/docker-/}"
